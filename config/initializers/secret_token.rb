f3b2cb543cf8d7180f9d2eefc36e562c0801f79c
Julie Perilla <julie.perilla@gmail.com>
2011-10-15 15:10:28 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryHouse::Application.config.secret_token = 'd6fccfd7132ad25972471c2e74b8e56e9ac470c0878aa6f96f3978195b11a13e1b7a2804dc1402eca181f69f244c0e939ac0e80c7c6f29a04c7cac6b9da73d85'
